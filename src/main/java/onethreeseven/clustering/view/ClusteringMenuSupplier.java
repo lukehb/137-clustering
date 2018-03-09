3220776073bbe570a119969b946ac5902ee445fa
Luke <llbermingham@gmail.com>
2018-03-09 21:46:08 +1000

    Slight changes to clustering menu supplier due to refactors

--- src/main/java/onethreeseven/clustering/view/ClusteringMenuSupplier.java
+++ src/main/java/onethreeseven/clustering/view/ClusteringMenuSupplier.java
@@ -9,10 +9,9 @@ import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
 import onethreeseven.clustering.view.controller.KMeansViewController;
 import onethreeseven.trajsuitePlugin.model.BaseTrajSuiteProgram;
-import onethreeseven.trajsuitePlugin.view.AbstractMenuBarPopulator;
-import onethreeseven.trajsuitePlugin.view.MenuSupplier;
-import onethreeseven.trajsuitePlugin.view.TrajSuiteMenu;
-import onethreeseven.trajsuitePlugin.view.TrajSuiteMenuItem;
+import onethreeseven.trajsuitePlugin.view.*;
+
+import javax.swing.plaf.ViewportUI;
 import java.io.IOException;
 import java.net.URL;
 
@@ -35,33 +34,7 @@ public class ClusteringMenuSupplier implements MenuSupplier {
     }
 
     private Runnable makeView(Stage primaryStage, String resource, String title){
-        return () -> {
-
-            URL res = ClusteringMenuSupplier.class.getResource(resource);
-
-            if(res == null){
-                throw new IllegalArgumentException(resource + " view fxml resource could not be found.");
-            }
-
-            try {
-
-                FXMLLoader loader = new FXMLLoader(res);
-
-                Parent view = loader.load();
-                Stage stage = new Stage();
-                stage.initModality(Modality.NONE);
-                //stage.setAlwaysOnTop(true);
-                stage.initStyle(StageStyle.UTILITY);
-                stage.setTitle(title);
-                stage.setScene(new Scene(view));
-                stage.initOwner(primaryStage);
-                stage.show();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        };
+        return () -> ViewUtil.loadUtilityView(ClusteringMenuSupplier.class, primaryStage, title, resource);
     }
 
     private Runnable makeKMeansMenuItem(Stage primaryStage){
